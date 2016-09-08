503061fda26d1afb0728a49849f3fa0115eeee9e
Werner Smit <werner@evine.co>
2016-09-08 11:53:01 +0200

    Mock added

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -16,7 +16,14 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 
   public function test_wcct_get_option()
   {
+      $get_option = $this->getFunctionMock('WCCT', 'get_option');
+
+      $get_option->expects($this->once())
+      ->with($this->equalsTo('wcct_venue'))
+      ->willReturn('river-club');
+
       $this->assertEquals('river-club', wcct_get_option('venue'));
   }
+
 
 }
